4948b97b9603bc2ece64843bb5b09f826de8e3eb
“[firstname <[valid-email]>
2025-08-31 09:32:08 +0530

    https://github.com/annmariyamanimala-gif/Ann-Mariya-Joseph.git

---
+++ add.js
@@ -0,0 +1,17 @@
+function add(x,y)
+{
+    console.log(x+y);
+}
+function sub(x,y)
+{
+    console.log(x-y);
+}
+function mul(x,y)
+{
+    console.log(x*y);
+}
+function div(x,y)
+{
+    console.log(x/y);
+}
+module.exports={ add,sub,mul,div };
